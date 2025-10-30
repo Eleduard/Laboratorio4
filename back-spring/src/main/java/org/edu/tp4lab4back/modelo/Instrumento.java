01bc36fe919cd60c421eaaf860e0910dfa134c7a
Eduardo Daniel Martinez <eduardo.d.martinez@outlook.com>
2025-10-29 23:24:58 -0300

    modifica Instrumento para usar precio como Transient

--- back-spring/src/main/java/org/edu/tp4lab4back/modelo/Instrumento.java
+++ back-spring/src/main/java/org/edu/tp4lab4back/modelo/Instrumento.java
@@ -1,5 +1,8 @@
 package org.edu.tp4lab4back.modelo;
 
+import java.util.Comparator;
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,7 +21,6 @@ public class Instrumento {
     private String instrumento;
     private String modelo;
     private String imagen;
-    private double precio;
 
     @Column(name = "costo_envio")
     private String costoEnvio;
@@ -34,5 +36,24 @@ public class Instrumento {
     @ManyToOne
     @JoinColumn(name = "marca_id")
     private Marca marca;
+
+    @OneToMany(mappedBy = "instrumento")
+    private List<HistoricoPrecios> historicoPrecios;
+
+    @Transient
+    private Double precioActual;
+
+    public double getPrecioActual() {
+        if(precioActual != null) {
+            return precioActual;
+        }
+        if (historicoPrecios == null || historicoPrecios.isEmpty()) {
+            return 0.0;
+        }
+        return historicoPrecios.stream()
+                .max(Comparator.comparing(HistoricoPrecios::getFechaDesde))
+                .map(HistoricoPrecios::getPrecio)
+                .orElse(0.0);
+    }
 
 }
